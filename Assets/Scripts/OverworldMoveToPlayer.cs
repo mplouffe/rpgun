5374b08a76f7f45beb62086eab232ae94166a9be
Matheu Plouffe <matheuplouffe@gmail.com>
2020-03-24 21:47:24 -0500

    Added inverted player model for overworld enemy sprite. Added animation controller. WIP.

---
+++ Assets/Scripts/OverworldMoveToPlayer.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OverworldMoveToPlayer : MonoBehaviour
+{
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// This function is called every fixed framerate frame, if the MonoBehaviour is enabled.
+    /// </summary>
+    void FixedUpdate()
+    {
+        
+    }
+}
