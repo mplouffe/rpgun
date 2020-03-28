155a190209d9629b1ce508114eb1fcfde48110e4
Matheu Plouffe <matheuplouffe@gmail.com>
2020-03-27 22:39:23 -0500

    Started adding Fight Manager. WIP.

---
+++ Assets/Scripts/RPGun_FightManager.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RPGun_FightManager : MonoBehaviour
+{
+    private Dictionary<Guid, RPGun_Enemy> enemies;
+
+    private RPGun_GameManager manager;
+
+    void Awake()
+    {
+        manager = GameObject.FindGameObjectWithTag("GameManager").GetComponent<RPGun_GameManager>();
+    }
+
+    public Guid AddEnemy(RPGun_Enemy enemy)
+    {
+        Guid id = new Guid();
+        enemies.Add(id, enemy);
+        return id;
+    }
+
+    public void RemoveEnemy(Guid id)
+    {
+        enemies.Remove(id);
+        if (enemies.Count == 0)
+        {
+            manager.TriggerOverworld();
+        }
+    }
+}
