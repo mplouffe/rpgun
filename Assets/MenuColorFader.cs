7dfede9645b8b4972192e27c0795112b01a2188c
Matheu Plouffe <matheuplouffe@gmail.com>
2020-03-31 22:35:21 -0500

    Working on menu. WIP.

---
+++ Assets/MenuColorFader.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MenuColorFader : MonoBehaviour
+{
+    private int redValue;
+    private bool increasing;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        redValue = 0;
+        increasing = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (redValue < 50 && increasing)
+        {
+            redValue++;
+            Camera.main.backgroundColor = new Color(redValue, 0, 0);
+            if (redValue >= 50)
+            {
+                increasing = false;
+            }
+        } else if (redValue > 0 && !increasing)
+        {
+            redValue--;
+            Camera.main.backgroundColor = new Color(redValue, 0, 0);
+            if (redValue <= 0)
+            {
+                increasing = true;
+            }
+        }
+    }
+}
