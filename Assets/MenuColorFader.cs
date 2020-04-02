582e34e75af31573154bf0988af352c07c0cf535
Matheu Plouffe <matheuplouffe@gmail.com>
2020-04-01 20:32:30 -0500

    Fixed menu color fader.

--- Assets/MenuColorFader.cs
+++ Assets/MenuColorFader.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class MenuColorFader : MonoBehaviour
 {
-    private int redValue;
+    public float incrementValue;
+    private float redValue;
     private bool increasing;
 
     // Start is called before the first frame update
@@ -12,22 +13,23 @@ public class MenuColorFader : MonoBehaviour
     {
         redValue = 0;
         increasing = true;
+        incrementValue = float.IsNaN(incrementValue) ? 0.01f : incrementValue;
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (redValue < 50 && increasing)
+        if (redValue < 0.5 && increasing)
         {
-            redValue++;
+            redValue += incrementValue;
             Camera.main.backgroundColor = new Color(redValue, 0, 0);
-            if (redValue >= 50)
+            if (redValue >= 0.5)
             {
                 increasing = false;
             }
         } else if (redValue > 0 && !increasing)
         {
-            redValue--;
+            redValue -= incrementValue;
             Camera.main.backgroundColor = new Color(redValue, 0, 0);
             if (redValue <= 0)
             {
